12ca34a44b7c0a4130c08ee953912458189319a0
Dénes Fehér <denes.m.feher@gmail.com>
2022-06-04 17:19:43 +0200

    Modify tests to use mocks

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -9,34 +9,42 @@ import static org.mockito.Mockito.*;
 public class GT4500Test {
 
   private GT4500 ship;
+  private TorpedoStore torpedoStore1;
+  private TorpedoStore torpedoStore2;
 
   @BeforeEach
   public void init() {
-    this.ship = new GT4500(
-        new TorpedoStore(10),
-        new TorpedoStore(10));
+    torpedoStore1 = mock(TorpedoStore.class);
+    torpedoStore2 = mock(TorpedoStore.class);
+    this.ship = new GT4500(torpedoStore1, torpedoStore2);
   }
 
   @Test
   public void fireTorpedo_Single_Success() {
     // Arrange
+    when(torpedoStore1.getTorpedoCount()).thenReturn(1);
+    when(torpedoStore1.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
     assertEquals(true, result);
+    verify(torpedoStore1).fire(1);
   }
 
   @Test
   public void fireTorpedo_All_Success() {
     // Arrange
+    when(torpedoStore1.getTorpedoCount()).thenReturn(1);
+    when(torpedoStore1.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
     assertEquals(true, result);
+    verify(torpedoStore1).fire(1);
   }
 
 }
